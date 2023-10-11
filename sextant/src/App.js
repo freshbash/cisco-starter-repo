e2167f42e9236b0c6581d00cea8b04c1c76ffede
Bhashwar <bhaswarsengupta@gmail.com>
2023-10-11 06:42:47 +0530

    created banner and exhibit components

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
+//Banner component
+function Banner() {
+	return (
+		<div id="banner">
+			<h1>Sextant</h1>
+		</div>
+	)
+}
+
+//Component to contain all the metrics
+const Exhibit = () => {
+	return (
+		<div id="exhibit">
+			<h3>System Performance Metrics</h3>
+			<div id="metric-grid"></div>
+		</div>
+	)
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  	return (
+		<div>
+			<Banner />
+			<Exhibit />
+		</div>		
+  	);
 }
 
 export default App;
